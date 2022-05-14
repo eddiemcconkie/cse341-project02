3133f099865dc10269c9750552ab2a9f46594d7a
Eddie McConkie <edwardmcconkie@gmail.com>
2022-05-14 17:42:16 -0600

    Added Access-Control headers

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const port = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  )
+  res.setHeader('Application-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  next()
+})
 app.use('/contacts', contactsRouter)
 app.use(
   '/api-docs',
